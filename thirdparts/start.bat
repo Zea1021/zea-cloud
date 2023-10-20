748a7dc4e9c9c926b312a99ca8fb35ec647c9018
zea <zea1021@163.com>
2023-10-20 11:11:01 +0800

    更新三方组件批量启动程序

--- thirdparts/start.bat
+++ thirdparts/start.bat
@@ -1,27 +1,26 @@
-@echo off
-timeout /t 1
+set PROJECT_PATH=D:\Code\Private\zea-cloud
 
-cd /d D:\Code\Private\zea-cloud\thirdparts\nacos-2.2.1\bin
+cd %PROJECT_PATH%\thirdparts\nacos-2.2.1\bin
 start  startup.cmd
 timeout /t 5
 
-cd /d D:\Code\Private\zea-cloud\thirdparts\redis
+cd %PROJECT_PATH%\thirdparts\redis
 start redis-server-start.bat
 timeout /t 1
 
-cd /d D:\Code\Private\zea-cloud\thirdparts\minio
+cd %PROJECT_PATH%\thirdparts\minio
 start startMinio.bat
 timeout /t 1
 
-cd /d D:\Code\Private\zea-cloud\thirdparts\xxl-job
+cd %PROJECT_PATH%\thirdparts\xxl-job
 start  startup.bat
 timeout /t 1
 
-cd /d D:\Code\Private\zea-cloud\thirdparts\rocketmq\rocketmq-all-5.1.3-bin-release
+cd %PROJECT_PATH%\thirdparts\rocketmq\rocketmq-all-5.1.3-bin-release
 start  startup.bat
 timeout /t 1
 
-cd /d D:\Code\Private\zea-cloud\thirdparts\seata-1.6.1\bin
+cd %PROJECT_PATH%\thirdparts\seata-1.6.1\bin
 start  seata-server.bat
 timeout /t 1
 
